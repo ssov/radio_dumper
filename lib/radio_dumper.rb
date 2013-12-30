d029027a68f1d3dca2963e6a960237b37ffb1cac
Toshiyuki Shimanuki <happy.new@nyaa.info>
2013-12-30 13:57:44 +0900

    Implemented radiko

--- lib/radio_dumper.rb
+++ lib/radio_dumper.rb
@@ -1,5 +1,54 @@
 require "radio_dumper/version"
 
 module RadioDumper
-  # Your code goes here...
+  class RadioDumper::Error
+    class Plugin
+      class NotFound < Exception; end
+    end
+  end
+
+  class << self
+    class Configure
+      attr_accessor :directory, :time
+    end
+    @@config = nil
+
+    def output(format)
+      time = Time.now
+      "#{@@config.directory}/#{format}-#{time.strftime("%Y-%m-%d-%H-%M")}.flv"
+    end
+
+    def dump(plugin, options)
+      @@plugin = plugin
+      Plugin.eval(plugin, options)
+    end
+
+    def configure(&block)
+      @@config = Configure.new
+      block.call(@@config)
+    end
+
+    def config
+      @@config
+    end
+  end
+
+  class Plugin
+    class << self
+      @@plugins = {}
+      def create(name, &block)
+        @@plugins[name] = block
+      end
+
+      def eval(name, options)
+        name = name.to_sym
+        raise RadioDumper::Error::Plugin::NotFound unless @@plugins.member?(name)
+        @@plugins[name].call
+        dump(options)
+      end
+    end
+  end
+
+  # load plugins
+  Dir.glob(File.expand_path("./lib/radio_dumper/plugins") + "/*.rb"){|file| require file}
 end
